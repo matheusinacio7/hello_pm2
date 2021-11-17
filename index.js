acf5f4c6449970ea53dbd0603aa5ca0d56a97000
heyset <c.inaciomatheus@gmail.com>
2021-11-17 19:05:12 -0300

    Adiciona codigo para testar tamanho de memoria

--- index.js
+++ index.js
@@ -9,12 +9,35 @@ const sleep = (time) => new Promise((resolve) => {
   }, time);
 });
 
+class BigArray {
+  constructor() {
+    this.arr = [];
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+
+  increaseArraySizeBy(sizeIncrease) {
+    for (let i = 0; i < sizeIncrease; i++) {
+      this.arr.push(Math.random() * 10000000);
+    }
+  }
+}
+
 (async () => {
-  await sleep(5000);
+  const myBigArray = new BigArray();
 
   app.get('/', (_req, res) => {
-    res.status(200).json({ message: 'Hello PM2!' });
+    res.status(200).json({ message: 'Hello PM2!', arraySize: myBigArray.length });
   });
+
+  app.get('/grow/:amount', (req, res) => {
+    const { amount } = req.params;
+    myBigArray.increaseArraySizeBy(amount);
+    console.log(`Increased array size by ${amount}`);
+    res.status(200).json({ message: `Increased array size by ${amount}` });
+  })
   
   app.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}.`);
