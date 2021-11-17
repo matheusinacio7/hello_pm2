b23f5d2d8d7171dc072ec56e48345f546ea2f004
heyset <c.inaciomatheus@gmail.com>
2021-11-17 18:38:50 -0300

    Tentativa de lazy init

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.get('/', (_req, res) => {
-  res.status(200).json({ message: 'Hello PM2!' });
+const sleep = (time) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, time);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is up on port ${PORT}.`);
-});
+(async () => {
+  await sleep(5000);
+
+  app.get('/', (_req, res) => {
+    res.status(200).json({ message: 'Hello PM2!' });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}.`);
+  });
+})();
